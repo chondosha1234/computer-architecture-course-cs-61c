fb9ae4e49f2d460e674519682d2123fd33327b0d
chondosha1234 <jmiller61193@gmail.com>
2020-09-10 21:55:50 +0100

    finished all lab assignments

--- lab02/lfsr.c
+++ lab02/lfsr.c
@@ -5,6 +5,11 @@
 #include "lfsr.h"
 
 void lfsr_calculate(uint16_t *reg) {
-    /* YOUR CODE HERE */
+	uint16_t next = *reg & 1;
+	next = next ^ ((*reg >> 2) & 1);
+	next = next ^ ((*reg >> 3) & 1);
+	next = next ^ ((*reg >> 5) & 1);
+	*reg = *reg >> 1;
+	*reg ^= (-next ^ *reg) & (1 << 15);
 }
 
